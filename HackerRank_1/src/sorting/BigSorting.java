089af0bd896cb437ba889f42c9c53ef721e8b2c1
Anil Parikipandla <anil_Parikipandla@epam.com>
2017-09-19 20:40:11 +0530

    Added sorting package and DivisibleSumPairs

---
+++ HackerRank_1/src/sorting/BigSorting.java
@@ -0,0 +1,19 @@
+package sorting;
+
+import java.util.Scanner;
+
+public class BigSorting {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		String[] unsorted = new String[n];
+		for(int unsorted_i=0; unsorted_i < n; unsorted_i++){
+			unsorted[unsorted_i] = in.next();
+		}
+		for
+		(int i = 0; i < n; i++){
+			
+		}
+	}
+
+}
